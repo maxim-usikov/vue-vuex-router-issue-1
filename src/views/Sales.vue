5ca6ee8ad1506c3529f67abb3e78661d5318a702
Maxim Usikov <mu.usikov@gmail.com>
2018-12-16 12:10:56 +0300

    feat: add `Shops`, `Sales` views.

---
+++ src/views/Sales.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Sales</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sales',
+};
+</script>
---
+++ src/views/Shops.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Shops</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Shops',
+};
+</script>
