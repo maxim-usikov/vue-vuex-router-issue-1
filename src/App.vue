62fecdace0b64bb72df969db4809b7611e7d756a
Maxim Usikov <mu.usikov@gmail.com>
2018-12-16 12:30:00 +0300

    chore: change for using dynamic component.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <AppLayout />
+    <Component :is="'AppLayout'" />
   </div>
 </template>
 
@@ -18,7 +18,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
